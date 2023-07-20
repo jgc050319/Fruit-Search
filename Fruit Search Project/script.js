6bd3fde896da44c8ae6bfe37e1d9b36c8eb7c6e8
jgc050319 <136496193+jgc050319@users.noreply.github.com>
2023-07-19 22:11:03 -0500

    Add files via upload

--- Fruit Search Project/script.js
+++ Fruit Search Project/script.js
@@ -2,27 +2,51 @@ const input = document.querySelector('#fruit');
 const suggestions = document.querySelector('.suggestions ul');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+let userInput="";
+let output=[];
 
-function search(str) {
-	let results = [];
-
-	// TODO
-
-	return results;
+for (let name of fruit) {
+	if (name.toLowerCase().includes(userinput.toLowerCase())){
+		output.push(name);
+	}
 }
+console.log(output);
 
-function searchHandler(e) {
-	// TODO
+function showFruit(arrayFruit){
+	let li=''
+	for(let name of arrayFruit){
+	li += `<li> ${fruit} </li>`;
+	}
+	ul.innerHTML = li;
 }
+userInput.addEventListener('keyup', function(e){
+let currentInput = e.target
+});
 
-function showSuggestions(results, inputVal) {
 
-	// TODO
-}
 
-function useSuggestion(e) {
-	// TODO
-}
 
-input.addEventListener('keyup', searchHandler);
-suggestions.addEventListener('click', useSuggestion);
+
+// function search(str) {
+// 	let results = [];
+
+// 	// TODO
+
+// 	return results;
+// }
+
+// function searchHandler(e) {
+// 	// TODO
+// }
+
+// function showSuggestions(results, inputVal) {
+
+// 	// TODO
+// }
+
+// function useSuggestion(e) {
+// 	// TODO
+// }
+
+// 
+// suggestions.addEventListener('click', useSuggestion);
