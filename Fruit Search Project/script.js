e5542882f3680fc04492666ffc50622d6b3a92b1
jgc050319 <136496193+jgc050319@users.noreply.github.com>
2023-07-25 18:23:58 -0500

    Add files via upload

--- Fruit Search Project/script.js
+++ Fruit Search Project/script.js
@@ -1,69 +1,79 @@
-const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 
+'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry',
+ 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa',
+  'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 
+  'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 
+  'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 
+  'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 
+  'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 
+  'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 
+  'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 
+  'Tamarind', 'Yuzu'];
+  const suggestions = document.querySelector(".Fruit");
+      const inputBox = document.querySelector("#inputBox");
 
-const suggestions = document.querySelector('.suggestions ul');
-const inputBox = document.querySelector('input-box');
-function searchContainer(arrayFruit){
-	(inputBox.toLowerCase().includes(currentInput));{
-	let result = [];
-	let input = inputBox.value;
-	if (input.length){
-		result = availableKeywords.filter((keyword)=>{
-			return keyword.toLowerCase().includes(input.toLowerCase());
-	});
-		console.log(result)
-	}
-	display(result);
-	if(!result.length){
-		resultsBox.innerHTML = '';
-	}
-}}
+      function showFruit(arrayFruit) {
+        let li = "";
+        for (let name of arrayFruit) {
+          li += `<li>${name}</li>`;
+        }
+        suggestions.innerHTML = li;
+        attachLiListeners(); // Attach event listeners to the newly created li elements
+      }
 
-function display(result){
-	const content = result.map((list)=>{
-		return "<li onclick=selectInput(this)>" + list + "</li>";
-	});
+      function display(result) {
+        const content = result.map((list) => {
+          return `<li>${list}</li>`;
+        });
+        return content.join("");
+      }
 
-	suggestions.innerHTML = "<ul>"+ content.join(" ") + "</ul>";
-}
+      function attachLiListeners() {
+        const liElements = suggestions.querySelectorAll("li");
+        liElements.forEach((li) => {
+          li.addEventListener("click", function () {
+            inputBox.value = li.textContent;
+            suggestions.innerHTML = "";
+          });
+        });
+      }
 
-function selectInput(list){
-	inputBox.value = list.innerHTML
-}
+      inputBox.addEventListener("input", function (e) {
+        let currentInput = e.target.value.toLowerCase();
+        let output = [];
+        if (currentInput.length > 0) {
+          output = fruit.filter((fruit) =>
+            fruit.toLowerCase().includes(currentInput)
+          );
+        }
+        showFruit(output);
+      });
 
 
-
-
-
-
-//FIRST ATTEMPT - Did not work
-// let userInput="";
-// let output=[];
-
-// for (let name of fruit) {
-// 	if (name.toLowerCase().includes(userinput.toLowerCase())){
-// 		output.push(name);
+// function searchContainer(arrayFruit){
+// 	(inputBox.toLowerCase().includes(currentInput));{
+// 	let result = [];
+// 	let input = inputBox.value;
+// 	if (input.length){
+// 		result = availableKeywords.filter((keyword)=>{
+// 			return keyword.toLowerCase().includes(input.toLowerCase());
+// 	});
+// 		console.log(result)
 // 	}
-// }
-// console.log(output);
-
-// function showFruit(arrayFruit){
-// 	let li=''
-// 	for(let name of arrayFruit){
-// 	li += `<li> ${fruit} </li>`;
+// 	display(result);
+// 	if(!result.length){
+// 		resultsBox.innerHTML = '';
 // 	}
-// 	ul.innerHTML = li;
+// }}
+
+
+
+// 	suggestions.innerHTML = "<ul>"+ content.join(" ") + "</ul>";
 // }
-// userInput.addEventListener('keyup', function(e){
-// let currentInput = e.target.value.toLowerCase();
-// let output= [];
-// output = fruit.filter((fruit=>
-// 	fruit.toLowerCase().includes(currentInput))
-// );
-// if(currentInput.length===0){
-// 	output=[];
+
+// function selectInput(list){
+// 	inputBox.value = list.innerHTML
 // }
-// showState(output);
-// });
 
 
 
